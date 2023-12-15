96d643d94909eb0c4ab2168a9627949ad02541df
IAmSnode <nuutti28@gmail.com>
2023-12-15 11:31:35 +0200

    Muokattu topappbarin värit ja lisäsin gameboardiin fontteja

--- app/src/main/java/com/example/bejeweled/ui/ScoreBoard.kt
+++ app/src/main/java/com/example/bejeweled/ui/ScoreBoard.kt
@@ -46,10 +46,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.example.bejeweled.ui.theme.ThemeOption
@@ -77,14 +79,15 @@ fun ScoreBoard(
 ) {
 
     val scoreboardListValue = remember { mutableStateListOf<ScoreboardInfo>() }
-    val database = Firebase.database("https://bejeweledmobiiliprojekti-default-rtdb.europe-west1.firebasedatabase.app/")
+    val database =
+        Firebase.database("https://bejeweledmobiiliprojekti-default-rtdb.europe-west1.firebasedatabase.app/")
     val scoreboardRef = database.getReference("scoreboardDetails")
     val query = scoreboardRef.orderByChild("score").limitToLast(25)
 
-    query.addValueEventListener (object : ValueEventListener {
+    query.addValueEventListener(object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
             val scoreboardList = mutableListOf<ScoreboardInfo>()
-            for(snapshot in snapshot.children){
+            for (snapshot in snapshot.children) {
                 scoreboardList.add(snapshot.getValue<ScoreboardInfo>()!!)
             }
             scoreboardList.reverse()
@@ -100,37 +103,7 @@ fun ScoreBoard(
             // ...
         }
     })
-    Scaffold (
-        topBar = {
-            TopAppBar(title = { Text(text = "Scoreboard") },
-                navigationIcon = {
-                    IconButton(
-                        onClick= { navController.navigate("start_menu")}  ,
-                        modifier = Modifier.padding(16.dp),
-                    ){
-                        Icon(Icons.Rounded.ArrowBack, contentDescription = "Back" )
-                    }
-                })
 
-        }
-    ){ innerPadding ->
-        ScoreboardList(
-            scoreboardList = scoreboardListValue,
-            selectedTheme = LIGHT,
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun ScoreboardList(
-    scoreboardList: List<ScoreboardInfo>,
-    selectedTheme: ThemeOption,
-    modifier: Modifier = Modifier
-) {
     val context = LocalContext.current
 
     var settings by remember { mutableStateOf(loadSettings(context)) }
@@ -139,22 +112,43 @@ fun ScoreboardList(
         saveSettings(context, settings)
     }
 
-    BejeweledTheme(selectedTheme = settings.theme) {gradient ->
+    BejeweledTheme(selectedTheme = settings.theme) { gradient ->
         val colorScheme = MaterialTheme.colorScheme
 
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Scoreboard") },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Color.Transparent,
+
+                    ),
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { navController.navigate("start_menu") },
+                            modifier = Modifier.padding(16.dp),
+                        )
+                        {
+                            Icon(Icons.Rounded.ArrowBack, contentDescription = "Back")
+                        }
+                    })
+            }
+        ) { innerPadding ->
             LazyColumn(
                 modifier = modifier
                     .fillMaxSize()
                     .background(gradient)
+                    .padding(innerPadding)
 
             ) {
-                items(scoreboardList) { scoreboardInfo ->
+                items(scoreboardListValue) { scoreboardInfo ->
                     ScoreboardCard(scoreboardInfo = scoreboardInfo)
                 }
             }
         }
-        
+
     }
+}
 
 
 @Composable
